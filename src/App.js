ed525130741017576cd106e9a7e618c60618fcef
Trung <ngolambaotrung@gmail.com>
2022-11-30 11:39:15 +0700

    Change the title of hero

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Footer from "./layouts/Footer";
 import Navbar from "./layouts/Navbar";
 import Form from "./components/Form";
-import Bakery from "./components/pics/bakery.jpg"
+import Bakery from "./components/pics/bakery.jpg";
 function App() {
   return (
     <div className="">
@@ -9,14 +9,16 @@ function App() {
       <main className=" bg-slate-200 card rounded-none">
         <div className="card rounded-none">
           <div className=" h-[400px] bg-slate-800 relative">
-            <img src={Bakery} className="mix-blend-overlay absolute object-cover w-full h-full"/>
+            <img
+              src={Bakery}
+              className="mix-blend-overlay absolute object-cover w-full h-full"
+            />
           </div>
           <div className="card-body absolute">
             <h2 className="card-title text-2xl sm:text-3xl md:text-4xl xl:text-5xl text-neutral-content">
-              Smarthome
+              SmartBakery's
             </h2>
             <p className="text-lg text-neutral-content">
-              Innovative Choice - Manage your living at ease with a button
             </p>
           </div>
         </div>
