7fade74af541843d8ebdda3d1f164dc13b6ab260
Trung <ngolambaotrung@gmail.com>
2022-11-30 11:47:45 +0700

    Modify hero section

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
       <Navbar />
       <main className=" bg-slate-200 card rounded-none">
         <div className="card rounded-none">
-          <div className=" h-[400px] bg-slate-800 relative">
+          <div className=" h-[400px] bg-slate-700 relative">
             <img
               src={Bakery}
               className="mix-blend-overlay absolute object-cover w-full h-full"
             />
           </div>
-          <div className="card-body absolute">
-            <h2 className="card-title text-2xl sm:text-3xl md:text-4xl xl:text-5xl text-neutral-content">
+          <div className="card-body absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 tracking-[5px] border-2 border-[#ccc] rounded-lg bg-[rgba(0,0,0,0.6)]">
+            <h2 className="card-title text-2xl sm:text-3xl md:text-4xl xl:text-6xl text-neutral-content ">
               SmartBakery's
             </h2>
             <p className="text-lg text-neutral-content">
